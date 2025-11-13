3daa21345dee4aeae7fe1158af1b7ec13f803829
Matthis Kruse <matthis.kruse@codasip.com>
2025-11-13 17:50:43 +0100

    fixup! [NVPTX] support switch statement with brx.idx (reland) (#102550)

--- llvm/include/llvm/CodeGen/TargetLowering.h
+++ llvm/include/llvm/CodeGen/TargetLowering.h
@@ -3955,7 +3955,7 @@ public:
   /// returned value is a member of the MachineJumpTableInfo::JTEntryKind enum.
   virtual unsigned getJumpTableEncoding() const;
 
-  virtual MVT getJumpTableRegTy(const DataLayout &DL) const {
+  virtual MVT getJumpTableRegTy(const DataLayout& DL) const {
     return getPointerTy(DL, DL.getDefaultGlobalsAddressSpace());
   }
 
