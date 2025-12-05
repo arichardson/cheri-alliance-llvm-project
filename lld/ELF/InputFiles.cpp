56f8e062ce14a926744bc8dfd12bb943800bf672
Matthis Kruse <matthis.kruse@codasip.com>
2025-12-05 12:49:00 +0100

    fixup! [ELF] Pass Ctx & to Arch/

--- lld/ELF/InputFiles.cpp
+++ lld/ELF/InputFiles.cpp
@@ -280,10 +280,6 @@ static bool isCompatible(Ctx &ctx, InputFile *file) {
         return true;
       onlyCheriAbi = true;
     }
-    if (ctx.arg.emachine != EM_MIPS)
-      return true;
-    if (isMipsN32Abi(ctx, *file) == ctx.arg.mipsN32Abi)
-      return true;
   }
 
   StringRef target =
