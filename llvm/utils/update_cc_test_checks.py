160316d99844d1c6c12bd70557fc67fea5799508
Matthis Kruse <matthis.kruse@codasip.com>
2026-01-08 19:21:25 +0100

    fixup! Handle leading underscores in update_cc_test_checks.py (#121800)

--- llvm/utils/update_cc_test_checks.py
+++ llvm/utils/update_cc_test_checks.py
@@ -27,10 +27,6 @@ import tempfile
 
 from UpdateTestChecks import common
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "lit"))
-from lit.TestRunner import ShellEnvironment, executeShCmd, ShellCommandResult
-from lit.ShUtil import ShParser, Pipeline, Command
-
 SUBST = {
     '%clang': [],
     '%clang_cc1': ['-cc1'],
@@ -49,19 +45,19 @@ SUBST = {
     '%riscv64_cheri_purecap_cc1': ['-cc1', "-triple=riscv64-unknown-freebsd", "-target-feature", "+xcheri", "-target-abi", "l64pc128", '-target-feature', '+cap-mode'],
     '%riscv32_cheri_purecap_clang': ['-target', 'riscv32-unknown-freebsd', '-march=rv32imafdcxcheri', '-mabi=il32pc64'],
     '%riscv64_cheri_purecap_clang': ['-target', 'riscv64-unknown-freebsd', '-march=rv64imafdcxcheri', '-mabi=l64pc128'],
-    '%riscv64_bakewell_hybrid_cc1': ['-cc1', '-triple=riscv64-unknown-freebsd', '-target-feature', '+zcherihybrid']
 }
+
 
 def get_line2func_list(args, clang_args, globals_name_prefix):
     ret = collections.defaultdict(list)
     # Use clang's JSON AST dump to get the mangled name
-    json_dump_args = clang_cmd.args + ['-fsyntax-only', '-o', '-']
-    if '-cc1' not in json_dump_args:
+    json_dump_args = [args.clang] + clang_args + ["-fsyntax-only", "-o", "-"]
+    if "-cc1" not in json_dump_args:
         # For tests that invoke %clang instead if %clang_cc1 we have to use
         # -Xclang -ast-dump=json instead:
-        json_dump_args.append('-Xclang')
-    json_dump_args.append('-ast-dump=json')
-    common.debug('Running', ' '.join(json_dump_args))
+        json_dump_args.append("-Xclang")
+    json_dump_args.append("-ast-dump=json")
+    common.debug("Running", " ".join(json_dump_args))
 
     popen = subprocess.Popen(
         json_dump_args,
@@ -260,11 +256,6 @@ def config():
             builtin_include_dir,
             "-nostdsysteminc",
         ]
-        tools = ["%riscv32_cheri_purecap_cc1", "%riscv64_cheri_purecap_cc1"]
-        for t in tools:
-            SUBST[t].extend(["-internal-isystem",
-                                         builtin_include_dir,
-                                         "-nostdsysteminc"])
     except subprocess.CalledProcessError:
         common.warn(
             "Could not determine clang builtins directory, some tests "
@@ -280,38 +271,50 @@ def config():
     return args, parser
 
 
-def execute_pipeline(commands: Pipeline) -> 'tuple[int, list[ShellCommandResult]]':
-    shenv = ShellEnvironment(os.getcwd(), os.environ)
-    results: 'list[ShellCommandResult]' = []
-    exitCode, _ = executeShCmd(commands, shenv, results)
-    return exitCode, results
-
-def get_function_body(builder, args, filename, clang_pipeline: 'list[Command]',
-                      prefixes):
+def get_function_body(
+    builder, args, filename, clang_args, extra_commands, prefixes, raw_tool_output
+):
     # TODO Clean up duplication of asm/common build_function_body_dictionary
-    # Invoke external tool and extract function bodies.
-    exitCode, results = execute_pipeline(Pipeline(clang_pipeline))
-    raw_tool_output = results[-1].stdout.replace('\r\n', '\n')
-    if '-emit-llvm' in clang_pipeline[0].args:
+    for extra_command in extra_commands:
+        extra_args = shlex.split(extra_command)
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(raw_tool_output.encode())
+            f.flush()
+            if extra_args[0] == "opt":
+                if args.opt is None:
+                    print(
+                        filename,
+                        "needs to run opt. " "Please specify --llvm-bin or --opt",
+                        file=sys.stderr,
+                    )
+                    sys.exit(1)
+                extra_args[0] = args.opt
+            raw_tool_output = common.invoke_tool(extra_args[0], extra_args[1:], f.name)
+    if "-emit-llvm" in clang_args:
         builder.process_run_line(
-            common.OPT_FUNCTION_RE, common.scrub_body, raw_tool_output,
-            prefixes
+            common.OPT_FUNCTION_RE, common.scrub_body, raw_tool_output, prefixes
         )
         builder.processed_prefixes(prefixes)
     else:
-        print('The clang command line should include -emit-llvm as asm tests '
-              'are discouraged in Clang testsuite.', file=sys.stderr)
+        print(
+            "The clang command line should include -emit-llvm as asm tests "
+            "are discouraged in Clang testsuite.",
+            file=sys.stderr,
+        )
         sys.exit(1)
 
 
-def exec_run_line(commands: Pipeline):
-    exitcode, results = execute_pipeline(commands)
-    if exitcode != 0:
-        sys.stderr.write('Failed to run ' + str(commands) + '\n')
-        for result in results:
-            sys.stderr.write(result.stderr)
-            sys.stderr.write(result.stdout)
+def exec_run_line(exe):
+    popen = subprocess.Popen(
+        exe, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
+    )
+    stdout, stderr = popen.communicate()
+    if popen.returncode != 0:
+        sys.stderr.write("Failed to run " + " ".join(exe) + "\n")
+        sys.stderr.write(stderr)
+        sys.stderr.write(stdout)
         sys.exit(3)
+
 
 def main():
     initial_args, parser = config()
@@ -328,77 +331,52 @@ def main():
         run_list = []
         line2func_list = collections.defaultdict(list)
 
-        subs = [
-          ('%s', ti.path),
-          ('%t', tempfile.NamedTemporaryFile().name),
-          ('%S', os.path.dirname(ti.path)),
-        ]
+        subs = {
+            "%s": ti.path,
+            "%t": tempfile.NamedTemporaryFile().name,
+            "%S": os.path.dirname(ti.path),
+        }
 
         for l in ti.run_lines:
-            pipeline = ShParser(l, win32Escapes=False, pipefail=False).parse()
-            if not isinstance(pipeline, Pipeline):
-                # Could be a sequence separated by &&/||/; but we don't handle that yet.
-                print('WARNING: RUN: line is too complex for this script: ', pipeline,
-                      file=sys.stderr)
-                continue
+            commands = [cmd.strip() for cmd in l.split("|")]
+
             triple_in_cmd = None
-            clang_cmd: Command = pipeline.commands[0]
-            m = common.TRIPLE_ARG_RE.search(' '.join(clang_cmd.args))
+            m = common.TRIPLE_ARG_RE.search(commands[0])
             if m:
                 triple_in_cmd = m.groups()[0]
 
-            # Do lit-like substitutions on the command and redirects.
-            for cmd in pipeline.commands:
-                if cmd.args[0] == 'opt':
-                    if ti.args.opt is None:
-                        sys.exit(ti.path + ' needs to run opt. '
-                                 'Please specify --llvm-bin or --opt')
-                    cmd.args[0] = ti.args.opt
-                cmd.args = [common.applySubstitutions(i, subs) for i in cmd.args]
-                for i, redirect in enumerate(cmd.redirects):
-                    cmd.redirects[i] = redirect[0], common.applySubstitutions(redirect[1], subs)
-
             # Parse executable args.
+            exec_args = shlex.split(commands[0])
             # Execute non-clang runline.
-            if clang_cmd.args[0] not in SUBST:
-                # Ignore FileCheck-only 'RUN: lines'
-                if pipeline.commands[0].args[0] == 'FileCheck':
-                  print('NOTE: Skipping FileCheck-only RUN line: ', pipeline, file=sys.stderr)
-                  continue
-                run_list.append((None, pipeline, None))
+            if exec_args[0] not in SUBST:
+                # Do lit-like substitutions.
+                for s in subs:
+                    exec_args = [
+                        i.replace(s, subs[s]) if s in i else i for i in exec_args
+                    ]
+                run_list.append((None, exec_args, None, None))
                 continue
             # This is a clang runline, apply %clang substitution rule, do lit-like substitutions,
             # and append args.clang_args
-            clang_cmd.args[0:1] = SUBST[clang_cmd.args[0]]
-            print(clang_cmd)
-            clang_cmd.args.insert(0, ti.args.clang)
-            clang_cmd.args += ti.args.clang_args
-            # Remove all -verify arguments since they could cause the IR generation to fail
-            clang_cmd.args = [x for x in clang_cmd.args if not x.startswith("-verify")]
+            clang_args = exec_args
+            clang_args[0:1] = SUBST[clang_args[0]]
+            for s in subs:
+                clang_args = [
+                    i.replace(s, subs[s]) if s in i else i for i in clang_args
+                ]
+            clang_args += ti.args.clang_args
 
             # Extract -check-prefix in FileCheck args
-            filecheck_cmd = ' '.join(pipeline.commands[-1].args)
+            filecheck_cmd = commands[-1]
             common.verify_filecheck_prefixes(filecheck_cmd)
-            if not filecheck_cmd.startswith('FileCheck ') and not filecheck_cmd.startswith('%cheri_FileCheck '):
+            if not filecheck_cmd.startswith("FileCheck "):
                 # Execute non-filechecked clang runline.
-                print('WARNING: Executing but ignoring non-filechecked RUN line: ' + l, file=sys.stderr)
-                run_list.append((None, pipeline, None))
-                continue
-            if '-ast-dump' in clang_cmd.args:
-                print('WARNING: Executing but ignoring -ast-dump RUN line: ' + l, file=sys.stderr)
-                run_list.append((None, pipeline, None))
-                continue
-            if '-fsyntax-only' in clang_cmd.args:
-                print('WARNING: Executing but ignoring -fsyntax-only RUN line: ' + l, file=sys.stderr)
-                run_list.append((None, pipeline, None))
-                continue
-            if '-emit-llvm' not in clang_cmd.args:
-                print('WARNING: Executing but ignoring assembly output RUN line: ' + l, file=sys.stderr)
-                run_list.append((None, pipeline, None))
+                exe = [ti.args.clang] + clang_args
+                run_list.append((None, exe, None, None))
                 continue
 
             check_prefixes = common.get_check_prefixes(filecheck_cmd)
-            run_list.append((check_prefixes, pipeline, triple_in_cmd))
+            run_list.append((check_prefixes, clang_args, commands[1:-1], triple_in_cmd))
 
         # Execute clang, generate LLVM IR, and extract functions.
 
@@ -413,17 +391,19 @@ def main():
             ginfo=ginfo,
         )
 
-        for prefixes, pipeline, triple_in_cmd in run_list:
-            assert isinstance(pipeline, Pipeline)
+        for prefixes, args, extra_commands, triple_in_cmd in run_list:
             # Execute non-filechecked runline.
             if not prefixes:
-                print('NOTE: Executing non-FileChecked RUN line: ', pipeline, file=sys.stderr)
-                exec_run_line(pipeline)
+                print(
+                    "NOTE: Executing non-FileChecked RUN line: " + " ".join(args),
+                    file=sys.stderr,
+                )
+                exec_run_line(args)
                 continue
 
-            clang_cmd = pipeline.commands[0:-1]
-            common.debug('Extracted clang cmd: clang {}'.format(clang_cmd))
-            common.debug('Extracted FileCheck prefixes: {}'.format(prefixes))
+            clang_args = args
+            common.debug("Extracted clang cmd: clang {}".format(clang_args))
+            common.debug("Extracted FileCheck prefixes: {}".format(prefixes))
 
             # Invoke external tool and extract function bodies.
             raw_tool_output = common.invoke_tool(ti.args.clang, clang_args, ti.path)
