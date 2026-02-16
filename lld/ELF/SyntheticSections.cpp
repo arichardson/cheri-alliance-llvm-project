eac5bab99551ec22dc2e43aad5a3416a0cf44d77
Lewis Belsten-Revill <lewis.belsten-revill@codasip.com>
2026-02-16 10:52:44 +0000

    Don't bypass addRelocCap when adding constant entries to GOT

--- lld/ELF/SyntheticSections.cpp
+++ lld/ELF/SyntheticSections.cpp
@@ -686,7 +686,7 @@ GotSection::GotSection()
   numEntries = target->gotHeaderEntriesNum;
 }
 
-void GotSection::addConstant(const Relocation &r) { relocations.push_back(r); }
+void GotSection::addConstant(const Relocation &r) { addReloc(r); }
 void GotSection::addEntry(const Symbol &sym) {
   // TODO: Separate out TLS IE entries for CHERI so we can pack them more
   // efficiently rather than consuming a whole capability-sized slot for an
